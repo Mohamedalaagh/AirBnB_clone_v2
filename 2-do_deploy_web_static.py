bdd6496039a4ebffc00a6485e45283b261a7020f
moalaa <moaladed4621@gmail.com>
2024-07-11 14:36:57 -0700

    moalaa

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,7 +7,7 @@ from datetime import datetime
 from fabric.api import *
 import os
 
-env.hosts = ["52.91.121.146", "3.85.136.181"]
+env.hosts = ["52.73.37.32", "34.207.212.203"]
 env.user = "ubuntu"
 
 
